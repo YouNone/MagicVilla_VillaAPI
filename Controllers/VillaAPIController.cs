6bc2931fc720be663cac67caac9e13620b2dc215
Alex <velengura.alex@gmail.com>
2024-01-08 20:07:22 +0200

    added post endpoint

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -7,17 +7,27 @@ namespace MagicVilla_VillaAPI.Controllers
 {
     //[Route("api/[controller]")]
     [Route("api/VillaAPI")]
-
+   
+ 
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult <IEnumerable<VillaDto>> GetVillas()
         {
             return Ok(VillaStore.villaList);
         }
 
+        //[ProducesResponseType(200, Type = typeof (VillaDto))]
+        //[ProducesResponseType(404)]
+        //[ProducesResponseType(400)]
         [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult <VillaDto> GetVilla(int id)
         {
             if (id == 0)
@@ -31,6 +41,29 @@ namespace MagicVilla_VillaAPI.Controllers
                 return NotFound();
             }
             return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<VillaDto> CreateVilla([FromBody]VillaDto villaDto)
+        {
+            if (villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+
+            if (villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villaDto.Id = VillaStore.villaList.OrderByDescending(item => item.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDto);
+
+            return Ok(villaDto);
         }
     }
 }

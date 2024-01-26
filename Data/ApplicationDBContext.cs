44327ca21181d2ef749e4039d7323fca56362aed
Alex <velengura.alex@gmail.com>
2024-01-26 15:11:12 +0200

    base options fix in ApplicationDBContext

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -5,7 +5,7 @@ namespace MagicVilla_VillaAPI.Data
 {
     public class ApplicationDBContext : DbContext
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
             
         }
         public DbSet<Villa> Villas {  get; set; }

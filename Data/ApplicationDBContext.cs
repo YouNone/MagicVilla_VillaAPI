037c8d1ab0abf0ddfe1386608aea2ed575969757
Alex <velengura.alex@gmail.com>
2024-02-15 20:04:45 +0200

    Added migration, VillaNumber

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -11,60 +11,61 @@ namespace MagicVilla_VillaAPI.Data
         }
         public DbSet<Villa> Villas {  get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Villa>().HasData(
-                new Villa
-                {
-                    Id = 1,
-                    Name = "First Villa 1",
-                    Details = "Some big Villa, with detailed info about it",
-                    ImageUrl = "https://cdn2.thecatapi.com/images/eda.jpg",
-                    Occupancy = 5,
-                    Rate = 200,
-                    Sqft = 550,
-                    Amenity = "",
-                    CreatedDate = DateTime.Now,
-                },
-                new Villa
-                {
-                    Id = 2,
-                    Name = "Second Villa 2",
-                    Details = "Smoller Villa, with less info about it",
-                    ImageUrl = "https://cdn2.thecatapi.com/images/752.jpg",
-                    Occupancy = 3,
-                    Rate = 100,
-                    Sqft = 300,
-                    Amenity = "",
-                    CreatedDate = DateTime.Now,
-                },
-                new Villa
-                {
-                    Id = 3,
-                    Name = "Third Villa 3",
-                    Details = "Medium Villa, with info about it",
-                    ImageUrl = "https://cdn2.thecatapi.com/images/5n9.jpg",
-                    Occupancy = 4,
-                    Rate = 400,
-                    Sqft = 450,
-                    Amenity = "",
-                    CreatedDate = DateTime.Now,
-                },
-                 new Villa
-                 {
-                     Id = 4,
-                     Name = "Forth Villa 4",
-                     Details = "Large Villa",
-                     ImageUrl = "https://cdn2.thecatapi.com/images/4qp.jpg",
-                     Occupancy = 6,
-                     Rate = 600,
-                     Sqft = 700,
-                     Amenity = "",
-                     CreatedDate = DateTime.Now,
-                 }
-                );
-            //base.OnModelCreating(modelBuilder);
-        }
+        public DbSet<VillaNumber> VillaNumber { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Villa>().HasData(
+        //        new Villa
+        //        {
+        //            Id = 1,
+        //            Name = "First Villa 1",
+        //            Details = "Some big Villa, with detailed info about it",
+        //            ImageUrl = "https://cdn2.thecatapi.com/images/eda.jpg",
+        //            Occupancy = 5,
+        //            Rate = 200,
+        //            Sqft = 550,
+        //            Amenity = "",
+        //            CreatedDate = DateTime.Now,
+        //        },
+        //        new Villa
+        //        {
+        //            Id = 2,
+        //            Name = "Second Villa 2",
+        //            Details = "Smoller Villa, with less info about it",
+        //            ImageUrl = "https://cdn2.thecatapi.com/images/752.jpg",
+        //            Occupancy = 3,
+        //            Rate = 100,
+        //            Sqft = 300,
+        //            Amenity = "",
+        //            CreatedDate = DateTime.Now,
+        //        },
+        //        new Villa
+        //        {
+        //            Id = 3,
+        //            Name = "Third Villa 3",
+        //            Details = "Medium Villa, with info about it",
+        //            ImageUrl = "https://cdn2.thecatapi.com/images/5n9.jpg",
+        //            Occupancy = 4,
+        //            Rate = 400,
+        //            Sqft = 450,
+        //            Amenity = "",
+        //            CreatedDate = DateTime.Now,
+        //        },
+        //         new Villa
+        //         {
+        //             Id = 4,
+        //             Name = "Forth Villa 4",
+        //             Details = "Large Villa",
+        //             ImageUrl = "https://cdn2.thecatapi.com/images/4qp.jpg",
+        //             Occupancy = 6,
+        //             Rate = 600,
+        //             Sqft = 700,
+        //             Amenity = "",
+        //             CreatedDate = DateTime.Now,
+        //         }
+        //        );
+        //}
 
     }
 }

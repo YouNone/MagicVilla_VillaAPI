816ca3a0b9815e0768de36cbbbc1145f0450d68a
Alex <velengura.alex@gmail.com>
2024-02-19 13:35:40 +0200

    Fixed VillaId issue

---
+++ Migrations/20240219105901_AddForeingKeyToVillaTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeingKeyToVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VillaId",
+                table: "VillaNumber",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VillaNumber_VillaId",
+                table: "VillaNumber",
+                column: "VillaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VillaNumber_Villas_VillaId",
+                table: "VillaNumber",
+                column: "VillaId",
+                principalTable: "Villas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VillaNumber_Villas_VillaId",
+                table: "VillaNumber");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VillaNumber_VillaId",
+                table: "VillaNumber");
+
+            migrationBuilder.DropColumn(
+                name: "VillaId",
+                table: "VillaNumber");
+        }
+    }
+}
